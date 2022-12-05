8669f87d2c62897d8543619132b34a6547c077f1
Charles Franklin Jahn <charles.jahn@gmail.com>
2022-12-05 13:34:15 +0000

    Login class create

---
+++ src/Authentication/Login.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Authentication;
+
+
+/**
+ *
+ * @author charl
+ */
+public class Login implements loginValidadeInterface {
+        
+    @Override
+    public boolean passwordValidade(String password, String realPassword) {
+        return password.equals(realPassword);
+    }
+    @Override
+    public boolean userValidade(String user, String realUser) {
+        return user.equals(realUser);
+    }
+    
+    @Override
+    public boolean loginValidate(String un,String up) {
+        return passwordValidade(un,"admin") && userValidade(up, "admin");
+    }
+}
---
+++ src/Authentication/loginValidadeInterface.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Authentication;
+
+/**
+ *
+ * @author Charles Franklin Jahn 2020315
+ */
+public interface loginValidadeInterface {
+
+    /**
+     *
+     * @param password
+     * @param realPassword
+     * @return true is param matchs
+     */
+    public boolean passwordValidade(String password, String realPassword);
+
+    /**
+     *
+     * @param password
+     * @param realPassword
+     * @return true is param matchs
+     */
+    public boolean userValidade(String password, String realPassword);
+
+    /**
+     *
+     * @param un user name
+     * @param up user password
+     * @return true if un and up are true
+     */
+    public boolean loginValidate(String un,String up );
+}
