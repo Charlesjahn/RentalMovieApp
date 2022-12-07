d1dd60d42e49d7df15ea057691a336812413d367
Charles Franklin Jahn <charles.jahn@gmail.com>
2022-12-07 16:56:26 +0000

    Update DatabaseCreating.java

--- src/CJDataBase/DatabaseCreating.java
+++ src/CJDataBase/DatabaseCreating.java
@@ -10,13 +10,22 @@ package CJDataBase;
  */
 public class DatabaseCreating {
 
+    private final DataBaseInfoUserPass dbInfo = new DataBaseInfoUserPass();
+    private final CJSaveInfoUserPass saveInfo = new CJSaveInfoUserPass();
+    private final String dbName = "users_passwords";
+    private final String DB_URL = "jdbc:mysql://localhost/" + dbName;
+    private final String USER = "CA1movieApp";
+    private final String PASS = "CA1movieApp";
+    private final String tableName = "UserPasswor";
+
     public void DatabaseCreating() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-        String dbName = "users_passwords";
-        String DB_URL = "jdbc:mysql://localhost/" + dbName;
-        String USER = "CA1movieApp";
-        String PASS = "CA1movieApp";
-        String tableName = "UserPasswor";
         DatabaseUsersPasswords userPassDB = new DatabaseUsersPasswords();
         userPassDB.dataBaseConn(dbName, USER, PASS, tableName);
+        dbInfo.DBInfoUserPass(DB_URL, USER, PASS, tableName);
+
+    }
+    public boolean saveInfo(String userName, String userPass){
+        saveInfo.insertInfoUserPass(userName, userPass, DB_URL, USER, PASS, tableName);
+        return true;
     }
 }
