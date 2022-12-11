9da510a4c086d48bd936c740900ddaff6f6082fa
Charles Franklin Jahn <charles.jahn@gmail.com>
2022-12-11 23:39:59 +0000

    Create DatabaseTableCreatingMovies.java

---
+++ src/GADataBase/DatabaseTableCreatingMovies.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GADataBase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author charl
+ */
+public class DatabaseTableCreatingMovies {
+    public void dataBaseConn(String dbName, String USER, String PASS, String tableName) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/", USER, PASS);
+            Statement stmt = conn.createStatement();
+            stmt.execute("CREATE SCHEMA IF NOT EXISTS " + dbName + ";");
+            stmt.execute("USE " + dbName + ";");
+            stmt.execute(
+                    "CREATE TABLE IF NOT EXISTS movies ("
+                    + "id INT NOT NULL PRIMARY KEY,"
+                    + "original_title VARCHAR(45) NOT NULL,"
+                    + "release_date VARCHAR(30) NOT NULL,"
+                    + "runtime VARCHAR(30) NOT NULL,"
+                    + "title VARCHAR(45) NOT NULL,"
+                    + "original_language VARCHAR(5) NOT NULL);"
+                    
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
