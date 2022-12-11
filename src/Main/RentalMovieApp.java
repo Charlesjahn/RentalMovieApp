3af8f6bb1335f2c0a5a33591606ae13765b141e8
Charles Franklin Jahn <charles.jahn@gmail.com>
2022-12-11 23:40:11 +0000

    Update RentalMovieApp.java

--- src/Main/RentalMovieApp.java
+++ src/Main/RentalMovieApp.java
@@ -1,12 +1,14 @@
 package Main;
 
-import DataBase.DatabaseMainProcessor;
+import CJDataBase.DatabaseMainProcessor;
 import CJAuthentication.Login;
 import CJAuthentication.loginValidadeInterface;
 import java.io.IOException;
 import java.util.Scanner;
-import CJ_GA_FileIO.CSVProcessor;
+import CJFileIO.CSVProcessor;
 import CJMenuLoginSign.LoopingMenuLoginSignValidation;
+import GADataBase.DatabaseTableCreatingMovies;
+import GAFile.SavingMovieTable;
 
 /**
  *
@@ -21,60 +23,61 @@ public class RentalMovieApp {
 //     * @throws java.lang.InstantiationException
 //     * @throws java.lang.IllegalAccessException
 //     */
-//    public static void main(String[] args) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
-//        Scanner myKB = new Scanner(System.in);
-//        loginValidadeInterface loginValidate = new Login();
-//        boolean userValidateLogin = false;
-//        int menuOption = 0;
-//        DatabaseMainProcessor databaseCreating = new DatabaseMainProcessor();
-//        databaseCreating.DatabaseCreating();
-////        databaseCreating.saveInfo("admin", "admin");
-//        databaseCreating.searchInfo("admin", "admin");
-//        /*
-//        * Here will Show the menu to login, Sign or Exir
-//        * While not insert valid input it will keep looping
-//        * This while will loop until be break with one of the option
-//         */
-//        while (menuOption == 0) {
-//            LoopingMenuLoginSignValidation loopMenuLoginSignVariable = new LoopingMenuLoginSignValidation();
-//            menuOption = loopMenuLoginSignVariable.loopMenuLoginSignValidation(menuOption);
-//
-//            switch (menuOption) {
-//                case 1:
-//                    System.out.println("User name: ");
-//                    String userName = myKB.nextLine();
-//                    System.out.println(userName);
-//                    System.out.println("Password: ");
-//                    String userPassword = myKB.nextLine();
-//                    System.out.println(userPassword);
-//                    if (!userValidateLogin) {
-//                        if (!loginValidate.loginValidate(userName, userPassword)) {
-//                            menuOption = 0;
-//                        }
-//                    }
-//                    System.out.println(menuOption);
-//                    break;
-//                case 2:
-//                    System.out.println("Under contrustion");
-//                    menuOption = 0;
-//                    break;
-//                case 3:
-//                    break;
-//            }
-//        }
-//        /*
-//        * If user is valid it here will be the menu for rent movies
-//         */
-//        if (userValidateLogin) {
-//            CSVProcessor csvProcessor = new CSVProcessor();
-//            csvProcessor.processCSV();
-//        }
-//    }
     public static void main(String[] args) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
-    
+
+        Scanner myKB = new Scanner(System.in);
+        loginValidadeInterface loginValidate = new Login();
+        boolean userValidateLogin = false;
+        int menuOption = 0;
         DatabaseMainProcessor databaseCreating = new DatabaseMainProcessor();
+        databaseCreating.setTableName("UserPasswor");
         databaseCreating.DatabaseCreating();
-        databaseCreating.loadMovieTable();
+//        databaseCreating.saveInfo("admin", "admin");
+        databaseCreating.searchInfo("admin", "admin");
+
+        SavingMovieTable databaseCreatingTableMove = new SavingMovieTable();
+        databaseCreating.setTableName("movies");
+        databaseCreating.DatabaseCreating();
+        databaseCreatingTableMove.loadMovieTable();
+        /*
+        * Here will Show the menu to login, Sign or Exir
+        * While not insert valid input it will keep looping
+        * This while will loop until be break with one of the option
+         */
+        while (menuOption == 0) {
+            LoopingMenuLoginSignValidation loopMenuLoginSignVariable = new LoopingMenuLoginSignValidation();
+            menuOption = loopMenuLoginSignVariable.loopMenuLoginSignValidation(menuOption);
+
+            switch (menuOption) {
+                case 1:
+                    System.out.println("User name: ");
+                    String userName = myKB.nextLine();
+                    System.out.println(userName);
+                    System.out.println("Password: ");
+                    String userPassword = myKB.nextLine();
+                    System.out.println(userPassword);
+                    if (!userValidateLogin) {
+                        if (!loginValidate.loginValidate(userName, userPassword)) {
+                            menuOption = 0;
+                        }
+                    }
+                    System.out.println(menuOption);
+                    break;
+                case 2:
+                    System.out.println("Under contrustion");
+                    menuOption = 0;
+                    break;
+                case 3:
+                    break;
+            }
+        }
+        /*
+        * If user is valid it here will be the menu for rent movies
+         */
+        if (userValidateLogin) {
+            CSVProcessor csvProcessor = new CSVProcessor();
+            csvProcessor.processCSV();
+        }
     }
 
 }
