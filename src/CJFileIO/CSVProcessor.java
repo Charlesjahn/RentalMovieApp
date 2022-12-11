17216aa3a0f8f84e53a70aa251e3ca81871b71f7
Charles Franklin Jahn <charles.jahn@gmail.com>
2022-12-11 23:40:02 +0000

    Create CSVProcessor.java

---
+++ src/CJFileIO/CSVProcessor.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CJFileIO;
+
+import GAFile.DataMapper;
+import GAFile.MovieRecords;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *
+ * @author Charles Franklin Jahn 2020315
+ */
+public class CSVProcessor {
+
+    public MovieRecords processCSV() throws IOException {
+        System.out.println("LOADING...");
+        String inputFile = "movies-test2.csv";
+        DataInput dataInput = new DataInput();
+        List<String> lines = dataInput.InputData(inputFile);
+        
+        DataMapper map = new DataMapper();
+        MovieRecords records = map.map(lines);
+        
+        return records;
+    }
+}
