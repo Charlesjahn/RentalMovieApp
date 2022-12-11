eb52e4fcbcaead7182b7acd0f8678f05cfd01cb4
Charles Franklin Jahn <charles.jahn@gmail.com>
2022-12-11 23:39:57 +0000

    Create DataInput.java

---
+++ src/CJFileIO/DataInput.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CJFileIO;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Charles Franklin Jahn 2020315
+ */
+public class DataInput {
+    public List<String> InputData(String InputFile) throws FileNotFoundException, IOException {
+        BufferedReader myReader = new BufferedReader(new FileReader(InputFile));
+        List<String> lines = new ArrayList<>();
+        String inputLine = myReader.readLine();
+        System.out.println(inputLine);
+        while ((inputLine = myReader.readLine()) != null) {
+            lines.add(inputLine);
+        }     
+        return lines;
+    }
+}
