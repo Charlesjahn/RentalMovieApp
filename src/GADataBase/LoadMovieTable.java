8d3086402b57f6d27c8c1530a169d1af20da1c7e
Gabriel <gabriel.pa16@gmail.com>
2022-12-12 15:15:11 +0000

    fixing db reload

--- src/GADataBase/LoadMovieTable.java
+++ src/GADataBase/LoadMovieTable.java
@@ -8,7 +8,9 @@ package GADataBase;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -18,20 +20,34 @@ public class LoadMovieTable {
     
     
     public void load(int id, String originalTitle, String releaseDate, String runtime, String title, String originalLanguage,String DB_URL, String USER, String PASS, String tableName) {
+        
+        
+        
         try {
-            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            String sql = "INSERT INTO movies (id, original_title, release_date, runtime, title, original_language) "
-                            + "VALUES (?,?,?,?,?,?);";
             
-            PreparedStatement preparedStmt = conn.prepareStatement(sql);
-            preparedStmt.setInt (1, id);
-            preparedStmt.setString (2, originalTitle);
-            preparedStmt.setString (3, releaseDate);
-            preparedStmt.setString (4, runtime);
-            preparedStmt.setString (5, title);
-            preparedStmt.setString (6, originalLanguage);
-            preparedStmt.execute();
-            conn.close();
+            
+            
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM users_passwords.movies;");
+            
+            if(rs.next() == false){
+            
+            
+                String sql = "INSERT INTO movies (id, original_title, release_date, runtime, title, original_language) "
+                                + "VALUES (?,?,?,?,?,?);";
+
+                PreparedStatement preparedStmt = conn.prepareStatement(sql);
+                preparedStmt.setInt (1, id);
+                preparedStmt.setString (2, originalTitle);
+                preparedStmt.setString (3, releaseDate);
+                preparedStmt.setString (4, runtime);
+                preparedStmt.setString (5, title);
+                preparedStmt.setString (6, originalLanguage);
+                preparedStmt.execute();
+                conn.close();
+            }
         
         } catch (SQLException e) {
             e.printStackTrace();
