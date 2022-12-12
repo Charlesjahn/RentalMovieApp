a120f4c4b95db9097f9e66fa7bc6e31866102685
Charles Franklin Jahn <charles.jahn@gmail.com>
2022-12-12 16:57:14 +0000

    Update DataBaseInfoUserPass.java

--- src/CJDataBase/DataBaseInfoUserPass.java
+++ src/CJDataBase/DataBaseInfoUserPass.java
@@ -1,6 +1,6 @@
 package CJDataBase;
 
-import CJMenuLoginSign.UserNamePassword;
+import CJMenuLoginSign.UserPasswordValidation;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -12,28 +12,38 @@ import java.sql.Statement;
  * @author Charles Franklin Jahn
  */
 public class DataBaseInfoUserPass {
-    private UserNamePassword info = new UserNamePassword();
+
+    private final UserPasswordValidation check = new UserPasswordValidation();
     private String name;
     private int idUser;
     private String passwordUser;
+//AND password = '" + userPass + "'
 
     public boolean DBInfoUserPass(String userName, String userPass, String DB_URL, String USER, String PASS, String tableName) {
         try {
             Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
             Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * from " + tableName + " WHERE userName = '" + userName + "' AND password = '" + userPass + "';");
+            ResultSet rs = stmt.executeQuery("SELECT * from " + tableName + " WHERE userName = '" + userName + "' ;");
             while (rs.next()) {
                 this.name = rs.getString("userName");
                 this.idUser = rs.getInt("idUser");
                 this.passwordUser = rs.getString("password");
-                System.out.println("Login Successful!!");
-                return true;
+                if (!check.validstionInfoDB(userName, this.name)) {
+                    System.out.println("User invalid");
+                    break;
+                }
+                if (!check.validstionInfoDB(userPass, this.passwordUser)) {
+                    System.out.println("Password invalid");
+                    break;
+                } else {
+                    return true;
+                }
             }
-            System.out.println("User invalid!!\nCheck your username and password");
-            return false;
         } catch (SQLException e) {
             return false;
         }
+        System.out.println("User invalid");
+        return false;
     }
 
     public String getName() {
