f7310549b3264693f58f6891ad7adf9b710d3105
Charles Franklin Jahn <charles.jahn@gmail.com>
2022-12-12 16:16:33 +0000

    Update DatabaseMainProcessor.java

--- src/CJDataBase/DatabaseMainProcessor.java
+++ src/CJDataBase/DatabaseMainProcessor.java
@@ -4,8 +4,6 @@
  */
 package CJDataBase;
 
-import GADataBase.DatabaseTableCreatingMovies;
-
 /**
  *
  * @author Charles Franklin Jahn
@@ -13,20 +11,19 @@ import GADataBase.DatabaseTableCreatingMovies;
 public class DatabaseMainProcessor {
 
     private final DatabaseTableCreatingUserPass createTable = new DatabaseTableCreatingUserPass();
-    private final DatabaseTableCreatingMovies moviesTable = new DatabaseTableCreatingMovies();
     private final DataBaseInfoUserPass dbInfo = new DataBaseInfoUserPass();
     private final SaveInfoUserPass saveInfo = new SaveInfoUserPass();
     private final String dbName = "users_passwords";
     private final String DB_URL = "jdbc:mysql://localhost/" + dbName;
-    private final String USER = "test";
-    private final String PASS = "";
+    private final String USER = "CA1movieApp";
+    private final String PASS = "CA1movieApp";
     private String tableName;
 
     public void DatabaseCreating() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         if ("UserPasswor".equals(tableName)) {
             createTable.dataBaseConnUser(dbName, USER, PASS, tableName);
         } else if ("movies".equals(tableName)) {
-            moviesTable.dataBaseConnMovies(dbName, USER, PASS, tableName);
+            createTable.dataBaseConnMovie(dbName, USER, PASS, tableName);
         }
     }
 
