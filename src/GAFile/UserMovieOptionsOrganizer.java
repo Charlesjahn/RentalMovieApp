f28a044fbf792422f7ef835c5b27a2f4468020bf
Gabriel <gabriel.pa16@gmail.com>
2022-12-20 13:03:13 +0000

    movie menu limitator

---
+++ src/GAFile/UserMovieOptionsOrganizer.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GAFile;
+
+import GADataBase.MovieDisplay;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author gabri
+ */
+public class UserMovieOptionsOrganizer {
+    
+    List<MovieDisplay> movies;
+    
+    public UserMovieOptionsOrganizer(List<MovieDisplay> movies){
+        this.movies = movies;
+    }
+    public void displayMovies(){
+        
+        List<MovieDisplay> temp = new ArrayList<MovieDisplay>();
+    
+        for(int i = 0; i < 10; i++){
+        
+            System.out.println(movies.get(i));
+            temp.add(movies.get(i));
+            movies.remove(i);
+            
+        
+        }
+        for(MovieDisplay md : temp){
+        
+            movies.add(md);
+            temp.remove(md);
+        }
+        
+        
+    
+    }
+    
+}
