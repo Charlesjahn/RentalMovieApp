ade0a8c82e00182be3f829045bccc287d6055882
Charles Franklin Jahn <charles.jahn@gmail.com>
2022-12-21 19:41:07 +0000

    Update FillingMovieTable.java

--- src/GADataBase/FillingMovieTable.java
+++ src/GADataBase/FillingMovieTable.java
@@ -17,37 +17,31 @@ import java.sql.Statement;
  * @Author Gabriel Antoniolli - Movie Database
  */
 public class FillingMovieTable {
-    
-    
-    public void load(int id, String originalTitle, String releaseDate, String runtime, String title, String originalLanguage,String DB_URL, String USER, String PASS, String tableName) {
-        
-        
-        
+
+    public void load(int id, String originalTitle, String releaseDate, String runtime, String title, String originalLanguage, String DB_URL, String USER, String PASS, String tableName) {
+
         try {
             Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            
+
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM users_passwords.movies;");
-            
-            if(rs.next() == false){
+
+            while (rs.next()) {
                 String sql = "INSERT INTO movies (id, original_title, release_date, runtime, title, original_language) "
-                                + "VALUES (?,?,?,?,?,?);";
+                        + "VALUES (?,?,?,?,?,?);";
 
                 PreparedStatement preparedStmt = conn.prepareStatement(sql);
-                preparedStmt.setInt (1, id);
-                preparedStmt.setString (2, originalTitle);
-                preparedStmt.setString (3, releaseDate);
-                preparedStmt.setString (4, runtime);
-                preparedStmt.setString (5, title);
-                preparedStmt.setString (6, originalLanguage);
+                preparedStmt.setInt(1, id);
+                preparedStmt.setString(2, originalTitle);
+                preparedStmt.setString(3, releaseDate);
+                preparedStmt.setString(4, runtime);
+                preparedStmt.setString(5, title);
+                preparedStmt.setString(6, originalLanguage);
                 preparedStmt.execute();
                 conn.close();
             }
-        
         } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println(e);
         }
     }
-    
+
 }
